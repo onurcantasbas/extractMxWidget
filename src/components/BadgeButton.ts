ad57d29479c651d01a9f9ca18303a3f60904d9d5
Onurcan TAŞBAŞ <onurcan.tasbas.cse@gmail.com>
2024-05-30 14:26:31 +0300

    init.

---
+++ src/components/BadgeButton.ts
@@ -0,0 +1,29 @@
+import { FC, createElement } from "react";
+import classNames from "classnames";
+
+import "../ui/BadgeButton.css";
+
+export interface BadgeButtonProps {
+    className?: string;
+    style?: object;
+    label?: string;
+    value?: string;
+    bootstrapStyle?: BootstrapStyle;
+    onClickAction?: () => void;
+    getRef?: (node: HTMLElement) => void;
+    defaultValue?: string;
+}
+
+export type BootstrapStyle = "default" | "info" | "primary" | "danger" | "success" | "warning";
+
+export const BadgeButton: FC<BadgeButtonProps> = (props) => createElement("button", {
+    className: classNames("widget-badge-button btn", props.className, {
+        [`btn-${props.bootstrapStyle}`]: !!props.bootstrapStyle
+    }),
+    onClick: props.onClickAction,
+    ref: props.getRef,
+    style: props.style
+},
+    createElement("span", { className: "widget-badge-button-text" }, props.label),
+    createElement("span", { className: "badge" }, props.value || props.defaultValue)
+);
